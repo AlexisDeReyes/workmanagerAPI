e75bc21b2e0d8a9776de3f2bec93a6633cfddb00
AlexisDeReyes <arreyes20285@gmail.com>
2019-12-18 15:31:35 -0500

    formatting and a few other changes

--- src/Types/TaskHolder.ts
+++ src/Types/TaskHolder.ts
@@ -1,86 +1,84 @@
-import DBItem from "./DBItem";
-import PrioritizedList from "./PrioritizedList";
-import { Task, TaskStatus } from "./Task";
+import DBItem from './DBItem';
+import PrioritizedList from './PrioritizedList';
+import { Task, TaskStatus } from './Task';
+
+const getNumberFromTaskStatus = (status: string): number => {
+  const stat = status as TaskStatus;
+
+  switch (stat) {
+    case TaskStatus.New:
+      return 0;
+    case TaskStatus.Ready:
+      return 1;
+    case TaskStatus.InProgress:
+      return 2;
+    case TaskStatus.Done:
+      return 3;
+    default:
+      return 4;
+  }
+};
 
 export default class TaskHolder extends DBItem {
-    public taskColumns: PrioritizedList[];
-    constructor() {
-        super();
-        this.taskColumns = [];
-    }
+  public taskColumns: PrioritizedList[];
+  constructor() {
+    super();
+    this.taskColumns = [];
+  }
 
-    public GetTask(identifier: string) {
-        let task: Task;
-        this.taskColumns.forEach((c) => {
-            if (!task) {
-                const columnTask = c.GetTask(identifier);
-                if (columnTask) {
-                    task = columnTask;
-                }
-            }
-        });
-
-        return task;
-    }
-
-    public AddTask(newTask: Task) {
-
-        this.GetColumn(TaskStatus.New).AddTask(newTask);
-        return newTask;
-    }
-
-    public RemoveTask(task: Task) {
-        const column = this.GetColumn(task.status);
-        column.RemoveTask(task);
-        if (column.tasks.length <= 0) {
-            this.taskColumns = this.taskColumns.filter((tc) => tc.name === column.name);
+  public GetTask(identifier: string) {
+    let task: Task;
+    this.taskColumns.forEach(c => {
+      if (!task) {
+        const columnTask = c.GetTask(identifier);
+        if (columnTask) {
+          task = columnTask;
         }
-        return task;
+      }
+    });
+
+    return task;
+  }
+
+  public AddTask(newTask: Task) {
+    this.GetColumn(TaskStatus.New).AddTask(newTask);
+    return newTask;
+  }
+
+  public RemoveTask(task: Task) {
+    const column = this.GetColumn(task.status);
+    column.RemoveTask(task);
+    if (column.tasks.length <= 0) {
+      this.taskColumns = this.taskColumns.filter(tc => tc.name === column.name);
     }
+    return task;
+  }
 
-    public ChangeTaskStatus(task: Task, newStatus: TaskStatus) {
+  public ChangeTaskStatus(task: Task, newStatus: TaskStatus) {
+    this.GetColumn(task.status).RemoveTask(task);
+    this.GetColumn(newStatus).AddTask(task);
+    task.status = newStatus;
+  }
 
-        this.GetColumn(task.status).RemoveTask(task);
-        this.GetColumn(newStatus).AddTask(task);
-        task.status = newStatus;
+  private sortColumns() {
+    this.taskColumns.sort(this.SortByColumnStatus);
+  }
+
+  private SortByColumnStatus(a: PrioritizedList, b: PrioritizedList): number {
+    const [first, second] = [a.name, b.name].map(getNumberFromTaskStatus);
+    return first - second;
+  }
+
+  private GetColumn(status: TaskStatus) {
+    const existingColumn = this.taskColumns.find(c => c.name === status);
+    if (existingColumn) {
+      return existingColumn;
+    } else {
+      const newColumn = new PrioritizedList(status);
+      this.taskColumns.push(newColumn);
+      this.sortColumns();
+
+      return newColumn;
     }
-
-    private sortColumns() {
-        this.taskColumns.sort(this.SortByColumnStatus);
-    }
-
-    private SortByColumnStatus(a: PrioritizedList, b: PrioritizedList): number {
-        const [first, second] = [a.name, b.name].map(this.getNumberFromTaskStatus);
-        return first - second;
-    }
-
-    private getNumberFromTaskStatus(status: string): number {
-        const stat = status as TaskStatus;
-
-        switch (stat) {
-            case TaskStatus.New:
-                return 0;
-            case TaskStatus.Ready:
-                return 1;
-            case TaskStatus.InProgress:
-                return 2;
-            case TaskStatus.Done:
-                return 3;
-            default:
-                return 4;
-        }
-    }
-
-    private GetColumn(status: TaskStatus) {
-        const existingColumn = this.taskColumns.find((c) => c.name === status);
-        if (existingColumn) {
-            return existingColumn;
-        } else {
-            const newColumn = new PrioritizedList(status);
-            this.taskColumns.push(newColumn);
-            this.sortColumns();
-
-            return newColumn;
-        }
-    }
+  }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,149 +1,169 @@
 // Imports
-import * as express from "express";
-import { Request, RequestHandler } from "express";
-import { CannedResponses, ICannedResponse, SendCannedResponse } from "./Types/CannedResponses";
-import { TeamTaskResponse } from "./Types/CompoundResources";
-import DBContext from "./Types/DBContext";
-import IQueryable from "./Types/IQueryable";
-import { Task, TaskStatus } from "./Types/Task";
-import Team from "./Types/Team";
+import * as express from 'express';
+import { Request, RequestHandler } from 'express';
+import { CannedResponses, ICannedResponse, SendCannedResponse } from './Types/CannedResponses';
+import { TeamTaskResponse } from './Types/CompoundResources';
+import DBContext from './Types/DBContext';
+import IQueryable from './Types/IQueryable';
+import { Task, TaskStatus } from './Types/Task';
+import Team from './Types/Team';
 
 // Setting up App
 const app = express();
 const port = 8080;
 app.use(express.json());
 
-app.all("*", (req, res, next) => {
-    res.set("Access-Control-Allow-Origin", "*");
-    res.set("Access-Control-Allow-Headers", ["Content-Type"]);
-    next();
+app.all('*', (req, res, next) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Headers', ['Content-Type']);
+  next();
 });
 
 // Data Storage
 const db = new DBContext();
 
 interface IRequestHandler {
-    handlerFunction: RequestHandler;
-    method: string;
-    route: string;
+  handlerFunction: RequestHandler;
+  method: string;
+  route: string;
 }
 
-const DoOrSendCannedResponse = (thing: any, response: ICannedResponse, req, res, stuffToDo: () => void) => {
-    if (thing) {
-        stuffToDo();
-    } else {
-        SendCannedResponse(response, req, res);
-    }
+const DoOrSendCannedResponse = (
+  thing: any,
+  response: ICannedResponse,
+  req,
+  res,
+  stuffToDo: () => void
+) => {
+  if (thing) {
+    stuffToDo();
+  } else {
+    console.error(JSON.stringify(response));
+    SendCannedResponse(response, req, res);
+  }
 };
 
-app.post("/teams", (req, res) => {
-    DoOrSendCannedResponse(db.GetTeam(req.body.name) === undefined,
-    CannedResponses.TeamNameExists, req, res, () => {
-        const team = new Team(req.body.name);
-        db.teams.push(team);
-        res.send(JSON.stringify(team));
+app.post('/teams', (req, res) => {
+  DoOrSendCannedResponse(
+    db.GetTeam(req.body.name) === undefined,
+    CannedResponses.TeamNameExists,
+    req,
+    res,
+    () => {
+      const team = new Team(req.body.name);
+      db.teams.push(team);
+      res.send(team);
+    }
+  );
+});
+
+app.put('/teams/:identifier', (req, res) => {
+  const team = db.GetTeam(req.params.identifier);
+
+  DoOrSendCannedResponse(team, CannedResponses.TeamDoesNotExist, req, res, () => {
+    const notSameName = team.name !== req.body.name;
+    const notDuplicateName = db.teams.every(t => t.name !== req.body.name);
+    const notDuplicateTeamNameAndNotSameName = notSameName && notDuplicateName;
+
+    DoOrSendCannedResponse(
+      notDuplicateTeamNameAndNotSameName,
+      CannedResponses.TeamNameExists,
+      req,
+      res,
+      () => {
+        team.name = req.body.name;
+        res.send(team);
+      }
+    );
+  });
+});
+
+app.get('/teams', (req, res) => {
+  res.send(db.teams);
+});
+
+app.get('/teams', (req, res) => {
+  res.send(db.teams.filter(team => team.Query(req.query.name.toLowerCase())));
+});
+
+app.get('/teams/:identifier', (req, res) => {
+  res.send(db.GetTeam(req.params.identifier));
+});
+
+app.post('/teams/:identifier/tasks', (req, res) => {
+  const team = db.GetTeam(req.params.identifier);
+
+  DoOrSendCannedResponse(team, CannedResponses.TeamDoesNotExist, req, res, () => {
+    const newTask: Task = Task.TaskFromRequest(req);
+    team.AddTask(newTask);
+    const response = new TeamTaskResponse(team, newTask);
+    res.send(response);
+  });
+});
+
+app.get('/teams/:identifier/tasks/:taskId', (req, res) => {
+  const team = db.GetTeam(req.params.identifier);
+
+  DoOrSendCannedResponse(team, CannedResponses.TeamDoesNotExist, req, res, () => {
+    const task = team.GetTask(req.params.taskId);
+
+    DoOrSendCannedResponse(task, CannedResponses.TaskDoesNotExist, req, res, () => {
+      res.send(task);
     });
+  });
 });
 
-app.put("/teams/:identifier", (req, res) => {
-    const team = db.GetTeam(req.params.identifier);
+app.delete('/teams/:identifier/tasks/:taskId', (req, res) => {
+  const team = db.GetTeam(req.params.identifier);
 
-    DoOrSendCannedResponse(team, CannedResponses.TeamDoesNotExist, req, res, () => {
-        const notSameName = team.name !== req.body.name;
-        const notDuplicateName = db.teams.every((t) => t.name !== req.body.name);
-        const notDuplicateTeamNameAndNotSameName = ( notSameName && notDuplicateName );
+  DoOrSendCannedResponse(team, CannedResponses.TeamDoesNotExist, req, res, () => {
+    const task = team.GetTask(req.params.taskId);
 
-        DoOrSendCannedResponse(notDuplicateTeamNameAndNotSameName,
-            CannedResponses.TeamNameExists, req, res,
-            () => {
-                team.name = req.body.name;
-                res.send(JSON.stringify(team));
-            });
+    DoOrSendCannedResponse(task, CannedResponses.TaskDoesNotExist, req, res, () => {
+      team.RemoveTask(task);
+      res.send(team);
     });
+  });
 });
 
-app.get("/teams", (req, res) => {
-    res.send(JSON.stringify(db.teams));
-});
+app.put('/teams/:identifier/tasks/:taskId', (req, res) => {
+  const team = db.GetTeam(req.params.identifier);
 
-app.get("/teams", (req, res) => {
-    res.send(JSON.stringify(db.teams.filter((team) => team.Query(req.query.name.toLowerCase()))));
-});
+  DoOrSendCannedResponse(team, CannedResponses.TeamDoesNotExist, req, res, () => {
+    const task = team.GetTask(req.params.taskId);
 
-app.get("/teams/:identifier", (req, res) => {
-    res.send(JSON.stringify(db.GetTeam(req.params.identifier)));
-});
-
-app.post("/teams/:identifier/tasks", (req, res) => {
-    const team = db.GetTeam(req.params.identifier);
-
-    DoOrSendCannedResponse(team, CannedResponses.TeamDoesNotExist, req, res, () => {
-        const newTask: Task = Task.TaskFromRequest(req);
-        team.AddTask(newTask);
-        const response = new TeamTaskResponse(team, newTask);
-        res.send(JSON.stringify(response));
+    DoOrSendCannedResponse(task, CannedResponses.TaskDoesNotExist, req, res, () => {
+      DoOrSendCannedResponse(req.body.name, CannedResponses.NeedName, req, res, () => {
+        task.UpdateFromRequest(req);
+        res.send(team);
+      });
     });
-
+  });
 });
 
-app.get("/teams/:identifier/tasks/:taskId", (req, res) => {
-    const team = db.GetTeam(req.params.identifier);
+app.patch('/teams/:identifier/tasks/:taskId/status/:status', (req, res) => {
+  const team = db.GetTeam(req.params.identifier);
 
-    DoOrSendCannedResponse(team, CannedResponses.TeamDoesNotExist, req, res, () => {
-        const task = team.GetTask(req.params.taskId);
+  DoOrSendCannedResponse(team, CannedResponses.TeamDoesNotExist, req, res, () => {
+    const task = team.GetTask(req.params.taskId);
 
-        DoOrSendCannedResponse(task, CannedResponses.TaskDoesNotExist, req, res, () => {
-            res.send(JSON.stringify(task));
-        });
+    DoOrSendCannedResponse(task, CannedResponses.TaskDoesNotExist, req, res, () => {
+      const status = req.params.status as TaskStatus;
+
+      DoOrSendCannedResponse(
+        Object.values(TaskStatus).includes(status),
+        CannedResponses.InvalidStatus,
+        req,
+        res,
+        () => {
+          team.ChangeTaskStatus(task, status);
+          res.send(team);
+        }
+      );
     });
-});
-
-app.delete("/teams/:identifier/tasks/:taskId", (req, res) => {
-    const team = db.GetTeam(req.params.identifier);
-
-    DoOrSendCannedResponse(team, CannedResponses.TeamDoesNotExist, req, res, () => {
-        const task = team.GetTask(req.params.taskId);
-
-        DoOrSendCannedResponse(task, CannedResponses.TaskDoesNotExist, req, res, () => {
-            team.RemoveTask(task);
-            res.send(JSON.stringify(team));
-        });
-    });
-});
-
-app.put("/teams/:identifier/tasks/:taskId", (req, res) => {
-    const team = db.GetTeam(req.params.identifier);
-
-    DoOrSendCannedResponse(team, CannedResponses.TeamDoesNotExist, req, res, () => {
-        const task = team.GetTask(req.params.taskId);
-
-        DoOrSendCannedResponse(task, CannedResponses.TaskDoesNotExist, req, res, () => {
-            DoOrSendCannedResponse(req.body.name, CannedResponses.NeedName, req, res, () => {
-                task.UpdateFromRequest(req);
-                res.send(JSON.stringify(team));
-            });
-        });
-    });
-});
-
-app.patch("/teams/:identifier/tasks/:taskId/status/:status", (req, res) => {
-    const team = db.GetTeam(req.params.identifier);
-
-    DoOrSendCannedResponse(team, CannedResponses.TeamDoesNotExist, req, res, () => {
-        const task = team.GetTask(req.params.taskId);
-
-        DoOrSendCannedResponse(task, CannedResponses.TaskDoesNotExist, req, res, () => {
-            const status = req.params.status as TaskStatus;
-
-            DoOrSendCannedResponse(Object.values(TaskStatus).includes(status),
-            CannedResponses.InvalidStatus, req, res, () => {
-                team.ChangeTaskStatus(task, status);
-                res.send(JSON.stringify(team));
-            });
-        });
-    });
+  });
 });
 
 app.listen(port, () => {
-    console.log(`Server Started on port ${port}`);
+  console.log(`Server Started on port ${port}`);
 });
